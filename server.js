6df7d31db0523961470ffed6a7aead46ee48462b
Giovani <desouzagiovani9@gmail.com>
2023-01-20 20:28:54 +0100

    modifié :         node_modules/.package-lock.json 	modifié :         package.json 	modifié :         server.js 	modifié :         client (contenu modifié)

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
-const port = process.env.port || 5000
 const cors = require('cors')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
 app.use(cors())
+app.use(express.static(__dirname + "/client/build"))
 
 app.get('/api', (req, res) => {
     res.json({
@@ -11,6 +13,6 @@ app.get('/api', (req, res) => {
         name: 'Giovani'
     })
 })
-app.listen(port, () => {
-    console.log(`server start on ${port}`)
+app.listen(PORT, () => {
+    console.log(`server start on ${PORT}`)
 })
